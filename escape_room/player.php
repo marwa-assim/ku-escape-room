8b8aa5c3f5b945c0a6bd5df797bab29877e67d60
marwa-assim <37294463+marwa-assim@users.noreply.github.com>
2026-09-24 16:46:47 +0300

    Delete escape_room directory

--- escape_room/player.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// player.php
-$pageTitle = 'Player Screen';
-$showNav   = false;                  // full-screen player display: no staff navbar
-
-require('views/players.phtml');
--- escape_room/puzzle.php
+++
@@ -1,6 +0,0 @@
-<?php
-// player.php
-$pageTitle = 'Sweet Puzzle Screen';
-$showNav   = false;                  // full-screen player display: no staff navbar
-
-require('views/puzzle.phtml');
